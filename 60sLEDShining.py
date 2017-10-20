7155dfa4e201e97701d9d34555abb3469a5de47d
hyhmnn <2827340173@qq.com>
2017-10-20 22:46:38 +0800

    Create 60sLEDShining.py

---
+++ 60sLEDShining.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+
+#init GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+def ledone():
+	try:
+		GPIO.output(11, 1)
+		time.sleep(0.3)
+		GPIO.output(11, 0)
+		time.sleep(0.3)
+	except:
+		print('...')
+def main():
+	for tmp_a in range(60):
+	#大概60s
+		ledone()
+	GPIO.cleanup()
+if __name__ == '__main__':
+	main()
