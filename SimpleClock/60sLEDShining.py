b6fd136044843586fb5244a1a53a7ab6e67f96ae
hyhmnn <2827340173@qq.com>
2018-07-02 01:44:34 +0800

    update simpleclock main.py

--- SimpleClock/60sLEDShining.py
+++ SimpleClock/60sLEDShining.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+#! /usr/bin/python3
+# coding:utf-8
+
 import RPi.GPIO as GPIO
 import time
 
--- SimpleClock/main.py
+++ SimpleClock/main.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+#! /usr/bin/python3
+# coding:utf-8
+
 import os
 import requests
 from aip import AipSpeech
@@ -7,10 +9,10 @@ class Clock():
 	def __init__(self):
 		self.city = "北京"
 		self.BASEDIR = os.path.dirname(os.path.abspath(__file__))
-        self.clockPath = os.path.join(self.BASEDIR, 'Clock.mp3')
+		self.clockPath = os.path.join(self.BASEDIR, 'Clock.mp3')
 
-	def getWeatherInfo(city):
-		api = "http://api.avatardata.cn/Weather/Query?key=[您申请的APPKEY]&cityname={0}".format(city)
+	def getWeatherInfo(self, city):
+		api = "http://api.avatardata.cn/Weather/Query?key=[---填写自己的key---]&cityname={0}".format(city)
 		response = requests.get(api)
 		info = response.json()
 		date = info["result"]["realtime"]["date"] + "星期" +info["result"]["realtime"]["week"]
@@ -23,15 +25,15 @@ class Clock():
 		resutl = "{date}，今天{areas}的温度是{temperature}度，{wind}，{remind}".format(date=date, areas=city, temperature=temperature, wind=wind, remind=remind)
 		return resutl
 
-	def stringToMp3(strings_txt):
+	def stringToMp3(self, strings_txt):
 		'''
 		用百度的AIP
 		把文字变成mp3文件
 		'''
 		strings_txt = '起床呀~要迟到啦!起床啦~要上课啦！今天是' + strings_txt
-		APPID = '[百度语音APPID]'
-		APIKey = '[百度语音APIKey]'
-		SecretKey = '[百度语音SecretKey]'
+		APPID = '[---填写自己的APPID---]'
+		APIKey = '[---填写自己的APIKey---]'
+		SecretKey = '[---填写自己的SecretKey---]'
 
 		aipSpeech = AipSpeech(APPID,APIKey,SecretKey)
 		result = aipSpeech.synthesis(strings_txt,'zh','1',\
@@ -42,9 +44,9 @@ class Clock():
 			with open(self.clockPath,'wb') as f:
 				f.write(result)
 
-	def run():
-		tmp = getWeatherInfo(self.city)
-		stringToMp3(tmp)
+	def run(self):
+		tmp = self.getWeatherInfo(self.city)
+		self.stringToMp3(tmp)
 		os.system('mplayer {0}'.format(self.clockPath))
 
 if __name__ == '__main__':
